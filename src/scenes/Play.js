514c62f0bdfe5c292e6c56b56225b4bbc66db20b
tommyn0225 <tommyn0225@gmail.com>
2025-02-07 15:35:50 -0800

    add UI borders update background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,10 +5,19 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // scrolling background
         this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0)
+
+        // UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0xFFFFFF).setOrigin(0, 0)
+        //white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0)
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0)
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0)
     }
 
     update() {
-        this.background.tilePositionX += 4
+        this.background.tilePositionX += 2
     }
 }
