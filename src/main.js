103f0024257f9e986f8bafaea73f3c68e92b166c
tommyn0225 <tommyn0225@gmail.com>
2025-02-10 23:27:00 -0800

    update main.js comment

--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ creative tilt justification:
 technical: Creating the random obstacle spawner was pretty tough. Making sure the rocks spawned
 in random Y coordinates of varying speeds and at various times while still keeping the game
 playable was a challenge. Balancing the obstacles while also making the game unpredictable
-and get increasingly more challenging took a lot of trial and error.
+and get increasingly more challenging took a lot of trial and error. I also added a high score feature.
 
-visual style: drawing with a mouse is hard...
+visual style: asteroids have trailing effects which took a lot of research and adds a bit of
+visual flair
 
 
 CREDITS
