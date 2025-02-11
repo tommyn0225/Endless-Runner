aec76155a97c3fa26303b8b761067acdddf4c66b
tommyn0225 <tommyn0225@gmail.com>
2025-02-10 22:10:54 -0800

    add: bgm + main.js comment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 //src/main.js
+/*
+Tommy Nguyen
+Space Runner
+~10 hours
+
+creative tilt justification:
+technical: Creating the random obstacle spawner was pretty tough. Making sure the rocks spawned
+in random Y coordinates of varying speeds and at various times while still keeping the game
+playable was a challenge. Balancing the obstacles while also making the game unpredictable
+and get increasingly more challenging took a lot of trial and error.
+
+visual style: drawing with a mouse is hard...
+
+
+CREDITS
+bgm: https://www.youtube.com/watch?v=ziFxpjRbfJc
+all art made with: https://www.pixilart.com/draw
+sfx made with: my mouth
+
+*/
 let config = {
     type: Phaser.AUTO,
     width: 640,
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,7 @@ class Menu extends Phaser.Scene {
         // load assets
         this.load.image('background', './assets/background.png')
         this.load.image('titleArt', './assets/titleart.png')
+        this.load.audio('bgm', './assets/bgm.mp3')
         this.load.spritesheet('asteroid', './assets/asteroid.png', {
             frameWidth: 64,
             frameHeight: 64,
@@ -19,8 +20,12 @@ class Menu extends Phaser.Scene {
     }
     
     create() {
-        // title screen background
+        // display title screen background
         this.add.image(game.config.width / 2, game.config.height / 2, 'titleArt').setOrigin(0.5)
+
+        // play background music
+        this.bgm = this.sound.add('bgm', { loop: true, volume: 0.10 });
+        this.bgm.play();
 
         // display menu text
         let menuConfig = {
@@ -40,7 +45,7 @@ class Menu extends Phaser.Scene {
         menuConfig.color = '#F00000'
         this.add.text(game.config.width / 2, game.config.height / 2 + 150, 'Press [LMouse] to start!', menuConfig).setOrigin(0.5)
         menuConfig.color = '#000000'
-        this.add.text(game.config.width / 2, game.config.height / 2 + 200, 'code, music, graphics by Tommy Nguyen', menuConfig).setOrigin(0.5)
+        this.add.text(game.config.width / 2, game.config.height / 2 + 200, 'code, sfx, graphics by Tommy Nguyen', menuConfig).setOrigin(0.5)
 
         // define keys
         this.keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
